708a79f32a6700e60cede60707e2976aa0a23183
Petrik Lajos Szg <info@petrik.hu>
2020-09-23 12:49:38 +0200

    10 szám bekérése a felhasználótól, ezeknek az átlaga kiíratása.

--- VerzioKezelesHF/Program.cs
+++ VerzioKezelesHF/Program.cs
@@ -10,6 +10,16 @@ namespace VerzioKezelesHF
     {
         static void Main(string[] args)
         {
+            int osszeg = 0;
+            for(int i =0;i<10;i++)
+            {
+                Console.WriteLine("Adja meg a(z) {0}. számot:",i+1);
+                osszeg += int.Parse(Console.ReadLine());
+            }
+            int atlag = osszeg / 10;
+            Console.WriteLine("A számok átlaga: {0}",atlag);
+
+            Console.ReadLine();
         }
     }
 }
